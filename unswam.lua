35d9ba9bd4960c94a719245e9e2c3fc7244b69b9
Sqxare <94962535+Sqxare@users.noreply.github.com>
2022-01-03 01:37:49 -0500

    Create unswam.lua

---
+++ unswam.lua
@@ -0,0 +1,9 @@
+local whitelistedspeds = {""} 
+--to add, just add a comma and proceed
+for loser, montero in pairs(whitelistedspeds) do
+if game.Players.LocalPlayer.UserId == montero then 
+print("whitelisted asf")
+else
+game.Players.LocalPlayer:Kick("SPED YOU BROKE SPED FUCK YOU BITCH") 
+end
+end
