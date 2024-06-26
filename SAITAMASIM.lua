e88c048b4a50b8b99c4c149c254dabaeb553cf45
Sqxare <94962535+Sqxare@users.noreply.github.com>
2024-06-25 22:36:00 -0400

    Update SAITAMASIM.lua

--- SAITAMASIM.lua
+++ SAITAMASIM.lua
@@ -1,3 +1,13 @@
+local hint = Instance.new("Hint", workspace)
+
+hint.Text = "Shouldn't have leaked the script LMAOOOO!"
+wait(3)
+game.Players.LocalPlayer:Kick("Leaking = No fun for anyone ;(")
+
+if workspace.Message then
+return
+end
+
 
 local gameName = ""
 
