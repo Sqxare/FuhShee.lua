436b44baf7930a6f4c55c45267ccf9e574c26ee9
Sqxare <94962535+Sqxare@users.noreply.github.com>
2024-06-25 22:29:29 -0400

    Update asdjkaldsj.lua

--- asdjkaldsj.lua
+++ asdjkaldsj.lua
@@ -1,4 +1,14 @@
 
+local hint = Instance.new("Hint", workspace)
+
+hint.Text = "Shouldn't have leaked the script LMAOOOO!"
+wait(3)
+game.Players.LocalPlayer:Kick("Leaking = No fun for anyone ;(")
+
+if workspace.Message then
+return
+end
+
 local gameName = ""
 
 if game.PlaceId == 6229116934 then
